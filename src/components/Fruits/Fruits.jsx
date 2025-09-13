035b054087b8a8481da3492af879b6e9dbf85a7d
Supratim Mandal <supratimm98@gmail.com>
2025-09-14 02:14:13 +0530

    Updated Theme

--- src/components/Fruits/Fruits.jsx
+++ src/components/Fruits/Fruits.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Categorypage from '../Categorypage/Categorypage'
-import Bgfruits from '../../assets/Fruits-banner.jpg'
+import Bgfruits from '../../assets/fruits-banner.jpg'
 
 const Fruits = () => {
   return (
